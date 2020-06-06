a3424589ac5bb9bf672ae08a2a1d10714dc2b5c8
miles <dkmk1149@gmail.com>
2020-06-06 18:04:08 -0400

    resets the grid

--- Scipts/scripts.js
+++ Scipts/scripts.js
@@ -1,5 +1,6 @@
 
 function createchart (){
+    //modify to allow the user to size the grid. For ex - Enters 16 -> creates a 16 by 16 grid
     const cont = document.querySelector(".container");
     for(let i = 0; i <= 255; i++){
         const div = document.createElement('div')
@@ -14,7 +15,11 @@ createchart();
         div.addEventListener("mouseover", () => div.style.backgroundColor = "black")
     });
     
-
+function reload(){
+    //functionality not created yet
+    let newgrid = prompt("how big do you want your new grid to be?");
+    window.location.reload();
+}
 
 
 
